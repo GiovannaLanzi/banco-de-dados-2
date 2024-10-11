f909c0cbb93ce61e4bf93f8a8b3e85d842b89fa7
Giovanna Lanzi Lopes Torres <160290107+GiovannaLanzi@users.noreply.github.com>
2024-10-11 20:50:13 -0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@ CREATE DATABASE aula03;
 use aula03;
 
 CREATE TABLE cliente(
-	i_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  /* i é para inteiro, o 1°cliente é o nome da coluna, e o 2° é o nome da tabela*/
+    i_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,  /* i é para inteiro, o 1°cliente é o nome da coluna, e o 2° é o nome da tabela*/
     s_nomecli_cliente VARCHAR(50) NOT NULL,
     s_cpf_cliente VARCHAR(11) NOT NULL,
     d_nasc_cliente DATETIME
